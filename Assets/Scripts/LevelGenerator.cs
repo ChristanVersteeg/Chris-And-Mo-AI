e7100669d9aba6b5421a1fad5201cf0f8d5be2f4
Wumpie7 <chrisjaver@gmail.com>
2023-02-25 22:23:23 +0100

    Added debug tools

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using Utils;
 using System.Collections.Generic;
+using System.Collections;
 
 public enum TileType
 {
@@ -20,26 +21,19 @@ public class LevelGenerator : MonoBehaviour
     public GameObject[] tiles;
     private const int gridWidth = 64, gridHeight = gridWidth;
     private const int maxRoomSizeX = 8, maxRoomSizeY = maxRoomSizeX;
-    private const int maxRooms = 25;
-    private List<Vector2> centers = new(), sizes = new();
+    private const int maxRooms = 100;
+    private Vector2 center, size;
+    private WaitForSeconds interval = new(0.1f);
 
 
     protected void Start()
     {
         TileType[,] grid = new TileType[gridHeight, gridWidth];
 
-        GenerateRooms(grid, maxRooms);
-        FillBlock(grid, 32, 28, 1, 1, TileType.Player);
-        FillBlock(grid, 30, 30, 1, 1, TileType.Dagger);
-        FillBlock(grid, 34, 30, 1, 1, TileType.Key);
-        FillBlock(grid, 32, 32, 1, 1, TileType.Door);
-        FillBlock(grid, 32, 36, 1, 1, TileType.Enemy);
-        FillBlock(grid, 32, 34, 1, 1, TileType.End);
-
-        CreateTilesFromArray(grid);
+        StartCoroutine(GenerateRooms(grid, maxRooms));
     }
 
-    private void GenerateRooms(TileType[,] grid, int roomCount)
+    private IEnumerator GenerateRooms(TileType[,] grid, int roomCount)
     {
         Random rand = new Random();
         int Rand(int val) => rand.Next(3, val);
@@ -56,9 +50,16 @@ public class LevelGenerator : MonoBehaviour
                 h = Rand(maxRoomSizeY);
             }
             Randomize();
+            center = new Vector2(x + w / 2, y + h / 2);
+            size = new Vector2(w, h);
 
             while (Physics2D.OverlapBoxAll(new Vector2(x + w / 2, y + h / 2), new Vector2(w, h), 0).Length > 0)
+            {
                 Randomize();
+                center = new Vector2(x + w / 2, y + h / 2);
+                size = new Vector2(w, h);
+                yield return interval;
+            }
 
             FillBlock(grid, x, y, w, h, TileType.Wall);
 
@@ -67,7 +68,18 @@ public class LevelGenerator : MonoBehaviour
             CreateTilesFromArray(grid);
 
             Debugger.instance.AddLabel(x + w / 2, y + h / 2, $"Room: {i} ");
+
+            yield return interval;
         }
+
+        FillBlock(grid, 32, 28, 1, 1, TileType.Player);
+        FillBlock(grid, 30, 30, 1, 1, TileType.Dagger);
+        FillBlock(grid, 34, 30, 1, 1, TileType.Key);
+        FillBlock(grid, 32, 32, 1, 1, TileType.Door);
+        FillBlock(grid, 32, 36, 1, 1, TileType.Enemy);
+        FillBlock(grid, 32, 34, 1, 1, TileType.End);
+
+        CreateTilesFromArray(grid);
     }
 
     //fill part of array with tiles 
@@ -126,9 +138,8 @@ public class LevelGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        for (int i = 0; i < centers.Count; i++)
-        {
-            Gizmos.DrawCube(centers[i], sizes[i]);
-        }
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireCube(center, size);
     }
 }
