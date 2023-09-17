a3438220a9eadd516a7b22c5fad915fceb72ec00
Wumpie7 <chrisjaver@gmail.com>
2023-09-17 10:27:40 +0200

    Removed unused variable

--- Assets/Scripts/Ingame/Replaceable.cs
+++ Assets/Scripts/Ingame/Replaceable.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Replaceable : MonoBehaviour
 {
     private LevelGenerator generator;
-    private int overlapCount, endRoomNum;
+    private int overlapCount;
 
     private void CheckOverlap(Vector3 direction) => overlapCount += Physics2D.OverlapBox(transform.position + direction, Vector2.one / 2, 0) == null ? 0 : 1;
 
